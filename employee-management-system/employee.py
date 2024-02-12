873a8c40d09399a539607d52ea5204c1e13853c3
cindyjeby <cindyjeby@gmail.com>
2024-02-12 11:35:08 +0000

    config

--- employee-management-system/employee.py
+++ employee-management-system/employee.py
@@ -2,6 +2,11 @@
 
 import mysql-connector
 
-db_config
-con = mysql.connector.connect(
-        host="localhost", user="root", password="password", database="emp")
+db_config = {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'my.password',
+        'database': 'employee_db',
+}
+
+con = mysql.connector.connect(**db_config)
