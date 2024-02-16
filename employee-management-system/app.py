18fb259c3eb0ffcd2d46f52e8af6d74dacfcbd3c
cindyjeby <cindyjeby@gmail.com>
2024-02-16 22:57:36 +0300

    endpoint connection

--- employee-management-system/app.py
+++ employee-management-system/app.py
@@ -1,10 +1,103 @@
-from flask import Flask, render_template
+#!/usr/bin/env python3
+
+from flask import Flask, request, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'my.password',
+    'database': 'employee_db',
+}
+
+con = mysql.connector.connect(**db_config)
+
+
+def check_employee(employee_id):
+    sql = 'select * from empd where id=%s'
+    c = con.cursor(buffered=True)
+    data = (employee_id,)
+    c.execute(sql, data)
+    r = c.rowcount
+    return r == 1
+
+
+def add_employee(data):
+    sql = 'insert into empd values(%s,%s,%s,%s)'
+    c = con.cursor()
+    c.execute(sql, data)
+    con.commit()
+    return "Employee Added Successfully"
+
+@app.route('/add_employee', methods=['POST'])
+def api_add_employee():
+    data = request.get_json()
+    if not check_employee(data['Id']):
+        return add_employee((data['Id'], data['Name'], data['Post'], data['Salary']))
+    else:
+        return "Employee already exists"
+
+def remove_employee(employee_id):
+    sql = 'delete from empd where id=%s'
+    data = (employee_id,)
+    c = con.cursor()
+    c.execute(sql, data)
+    con.commit()
+    return "Employee Removed"
+
+@app.route('/remove_employee/<int:employee_id>', methods=['DELETE'])
+def api_remove_employee(employee_id):
+    if check_employee(employee_id):
+        return remove_employee(employee_id)
+    else:
+        return "Employee does not exist"
+
+def promote_employee(employee_id, amount):
+    sql_select = 'select salary from empd where id=%s'
+    data_select = (employee_id,)
+    c = con.cursor()
+    c.execute(sql_select, data_select)
+    r = c.fetchone()
+    if r:
+        new_salary = r[0] + amount
+        sql_update = 'update empd set salary=%s where id=%s'
+        data_update = (new_salary, employee_id)
+        c.execute(sql_update, data_update)
+        con.commit()
+        return "Employee Promoted"
+    else:
+        return "Employee does not exist"
+
+@app.route('/promote_employee/<int:employee_id>', methods=['PUT'])
+def api_promote_employee(employee_id):
+    data = request.get_json()
+    amount = data.get('amount', 0)
+    if check_employee(employee_id):
+        return promote_employee(employee_id, amount)
+    else:
+        return "Employee does not exist"
+
+def display_employees():
+    sql = 'select * from empd'
+    c = con.cursor()
+    c.execute(sql)
+    employees = []
+    for i in c.fetchall():
+        employee = {
+            "id": i[0],
+            "name": i[1],
+            "post": i[2],
+            "salary": i[3]
+        }
+        employees.append(employee)
+    return jsonify(employees)
+
+@app.route('/display_employees', methods=['GET'])
+def api_display_employees():
+    return display_employees()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ employee-management-system/employee.js
@@ -0,0 +1,44 @@
+const apiUrl = "http://127.0.0.1:5000";
+
+function sendRequest(url, method, data = {}) {
+    return fetch(url, {
+        method,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .catch(error => console.error('Error:', error));
+}
+
+function addEmployee() {
+    const employeeData = {
+        Id: prompt("Enter Employee Id"),
+        Name: prompt("Enter Employee Name"),
+        Post: prompt("Enter Employee Post"),
+        Salary: prompt("Enter Employee Salary"),
+    };
+
+    sendRequest(apiUrl + '/add_employee', 'POST', employeeData)
+        .then(response => console.log(response));
+}
+
+function removeEmployee() {
+    const employeeId = prompt("Enter Employee Id to remove");
+    sendRequest(apiUrl + `/remove_employee/${employeeId}`, 'DELETE')
+        .then(response => console.log(response));
+}
+
+function promoteEmployee() {
+    const employeeId = prompt("Enter Employee Id to promote");
+    const amount = prompt("Enter increase in Salary");
+
+    sendRequest(apiUrl + `/promote_employee/${employeeId}`, 'PUT', { amount })
+        .then(response => console.log(response));
+}
+
+function displayEmployees() {
+    sendRequest(apiUrl + '/display_employees', 'GET')
+        .then(response => console.log(response));
+}
